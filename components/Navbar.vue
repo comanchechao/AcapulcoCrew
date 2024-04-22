99dcf9378ee53d53caa7b86f4fcee8dca1d8161c
Greez <imgreez@protonmail.com>
2024-04-22 10:40:43 +0330

    updates

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -46,6 +46,7 @@
         <LazyMButton class="Navbar opacity-0">
           <template #Text>Websites</template>
         </LazyMButton>
+        <w3m-button />
       </div>
       <img
         :onmouseleave="endAnimation"
@@ -62,6 +63,38 @@
 <script setup>
 import { PhCaretDown } from "@phosphor-icons/vue";
 import { onMounted } from "vue";
+
+import { createWeb3Modal, defaultWagmiConfig } from "@web3modal/wagmi";
+
+import { mainnet, arbitrum } from "viem/chains";
+import { reconnect } from "@wagmi/core";
+
+// 1. Define constants
+const projectId = "YOUR_PROJECT_ID";
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: "Web3Modal",
+  description: "Web3Modal Example",
+  url: "https://web3modal.com", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const chains = [mainnet, arbitrum];
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+});
+
+reconnect(config);
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true, // Optional - false as default
+});
 
 const { $gsap } = useNuxtApp();
 const TM = $gsap.timeline({ paused: false });
